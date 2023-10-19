cca30ef5db0ca2a76d1d697bef4d5381b4b41ebf
adamcseresznye <csenye.chem@gmail.com>
2023-10-19 20:59:00 +0200

    link updated

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ st.title("CP-Seeker Post-Processing App")
 
 st.markdown("""
             This app is designed to facilitate post-processing of CP-Seeker's output (McGrath, J. et al, 2023).   
-            Link to [GitHub](https://github.com/adamcseresznye/CPSeeker_post_processor) repository.
+            Link to [GitHub](https://github.com/adamcseresznye/CP-Seeker_post_processor) repository.
              **Disclaimer**:*The developer is not liable for errors or inaccuracies. This app is provided as-is, without any warranties or guarantees.*
 
             ## Steps:
